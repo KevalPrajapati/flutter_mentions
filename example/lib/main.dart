e1caaea1e49a1aea86e65a7ddc653e6ee3afaeb7
KevalPrajapati <keval.prajapatimus@gmail.com>
2021-12-26 00:25:33 +0530

    max lines

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      builder: (_, child) => Portal(child: child!),
+      builder: (_, child) => child!,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -34,103 +34,110 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title!),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          RaisedButton(
-            child: Text('Get Text'),
-            onPressed: () {
-              print(key.currentState!.controller!.markupText);
-            },
-          ),
-          Container(
-            child: FlutterMentions(
-              key: key,
-              suggestionPosition: SuggestionPosition.Top,
-              maxLines: 5,
-              minLines: 1,
-              decoration: InputDecoration(hintText: 'hello'),
-              mentions: [
-                Mention(
-                    trigger: '@',
+    return Portal(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title!),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            RaisedButton(
+              child: Text('Get Text'),
+              onPressed: () {
+                // print(key.currentState!.controller!.value);
+
+                print(key.currentState!.controller!.markupText);
+              },
+            ),
+            Container(
+              child: FlutterMentions(
+                key: key,
+                suggestionPosition: SuggestionPosition.Top,
+                maxLines: 500,
+                minLines: 1,
+                onChanged: (val) {
+                  print(key.currentState!.controller!.markupText);
+                },
+                decoration: InputDecoration(hintText: 'hello'),
+                mentions: [
+                  Mention(
+                      trigger: '@',
+                      style: TextStyle(
+                        color: Colors.amber,
+                      ),
+                      data: [
+                        {
+                          'id': '61as61fsa',
+                          'display': 'Fayeed Pawaskarx',
+                          'full_name': 'Fayeed Pawaskar',
+                          'photo':
+                              'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+                        },
+                        {
+                          'id': '61asasgasgsag6a',
+                          'display': 'khaled',
+                          'full_name': 'DJ Khaled',
+                          'style': TextStyle(color: Colors.purple),
+                          'photo':
+                              'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+                        },
+                        {
+                          'id': 'asfgasga41',
+                          'display': 'markT',
+                          'full_name': 'Mark Twain',
+                          'photo':
+                              'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+                        },
+                        {
+                          'id': 'asfsaf451a',
+                          'display': 'JhonL',
+                          'full_name': 'Jhon Legend',
+                          'photo':
+                              'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+                        },
+                      ],
+                      matchAll: false,
+                      suggestionBuilder: (data) {
+                        return Container(
+                          padding: EdgeInsets.all(10.0),
+                          child: Row(
+                            children: <Widget>[
+                              CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                  data['photo'],
+                                ),
+                              ),
+                              SizedBox(
+                                width: 20.0,
+                              ),
+                              Column(
+                                children: <Widget>[
+                                  Text(data['full_name']),
+                                  Text('@${data['display']}'),
+                                ],
+                              )
+                            ],
+                          ),
+                        );
+                      }),
+                  Mention(
+                    trigger: '#',
+                    disableMarkup: true,
                     style: TextStyle(
-                      color: Colors.amber,
+                      color: Colors.blue,
                     ),
                     data: [
-                      {
-                        'id': '61as61fsa',
-                        'display': 'fayeedP',
-                        'full_name': 'Fayeed Pawaskar',
-                        'photo':
-                            'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-                      },
-                      {
-                        'id': '61asasgasgsag6a',
-                        'display': 'khaled',
-                        'full_name': 'DJ Khaled',
-                        'style': TextStyle(color: Colors.purple),
-                        'photo':
-                            'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-                      },
-                      {
-                        'id': 'asfgasga41',
-                        'display': 'markT',
-                        'full_name': 'Mark Twain',
-                        'photo':
-                            'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-                      },
-                      {
-                        'id': 'asfsaf451a',
-                        'display': 'JhonL',
-                        'full_name': 'Jhon Legend',
-                        'photo':
-                            'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-                      },
+                      {'id': 'reactjs', 'display': 'reactjs'},
+                      {'id': 'javascript', 'display': 'javascript'},
                     ],
-                    matchAll: false,
-                    suggestionBuilder: (data) {
-                      return Container(
-                        padding: EdgeInsets.all(10.0),
-                        child: Row(
-                          children: <Widget>[
-                            CircleAvatar(
-                              backgroundImage: NetworkImage(
-                                data['photo'],
-                              ),
-                            ),
-                            SizedBox(
-                              width: 20.0,
-                            ),
-                            Column(
-                              children: <Widget>[
-                                Text(data['full_name']),
-                                Text('@${data['display']}'),
-                              ],
-                            )
-                          ],
-                        ),
-                      );
-                    }),
-                Mention(
-                  trigger: '#',
-                  disableMarkup: true,
-                  style: TextStyle(
-                    color: Colors.blue,
-                  ),
-                  data: [
-                    {'id': 'reactjs', 'display': 'reactjs'},
-                    {'id': 'javascript', 'display': 'javascript'},
-                  ],
-                  matchAll: true,
-                )
-              ],
+                    matchAll: true,
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
